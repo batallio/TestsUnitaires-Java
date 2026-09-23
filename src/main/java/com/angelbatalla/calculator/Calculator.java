53ed703f9bdb52cbf10622fd488d20e67d8c04ff
Angel David RAMIREZ BATALLA <contact@angelbatalla.com>
2026-09-23 15:11:36 +0200

    Functions

--- src/main/java/com/angelbatalla/calculator/Calculator.java
+++ src/main/java/com/angelbatalla/calculator/Calculator.java
@@ -1,4 +1,11 @@
 package com.angelbatalla.calculator;
 
 public class Calculator {
+
+    public static int add(int opG, int opD) {
+    }
+
+    public static int divide(int opG, int opD) {
+
+    }
 }
--- src/test/java/com/angelbatalla/calculator/CalculatorTest.java
+++ src/test/java/com/angelbatalla/calculator/CalculatorTest.java
@@ -3,5 +3,4 @@ package com.angelbatalla.calculator;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
 }

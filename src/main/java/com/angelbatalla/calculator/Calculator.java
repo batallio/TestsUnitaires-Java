035901a64e4ffe26399bca6a1e8e59b7a307081b
Angel David RAMIREZ BATALLA <contact@angelbatalla.com>
2026-09-23 16:17:30 +0200

    Functions implementation

--- src/main/java/com/angelbatalla/calculator/Calculator.java
+++ src/main/java/com/angelbatalla/calculator/Calculator.java
@@ -3,8 +3,8 @@ package com.angelbatalla.calculator;
 public class Calculator {
 
     public static int add(int opG, int opD) {
+        return opG + opD;
     }
 
-    public static int divide(int opG, int opD) {
-    }
+    public static int divide(int opG, int opD) { return opG / opD; }
 }

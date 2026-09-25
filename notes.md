3f3c6dff02ccc091bd34f076c13600b251329cee
Angel David RAMIREZ BATALLA <contact@angelbatalla.com>
2026-09-25 10:06:37 +0200

    Parameterized test for sum

---
+++ notes.md
@@ -0,0 +1,4 @@
+DIR
+
+JAVA_HOME
+C:\Users\adrbatalla\.jdks\corretto-18.0.2
--- src/test/java/com/angelbatalla/calculator/CalculatorTest.java
+++ src/test/java/com/angelbatalla/calculator/CalculatorTest.java
@@ -3,11 +3,13 @@ package com.angelbatalla.calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
-
     Calculator calc;
 
     @BeforeEach
@@ -44,6 +46,24 @@ class CalculatorTest {
 
         // WHEN
         int resultat_class = calc.divide(opG, opD);
+
+        // THEN
+        assertThat(resultat_class).isNotNull()
+                .isEqualTo(resultat_attendu);
+    }
+
+    // https://docs.junit.org/5.14.1/writing-tests/parameterized-classes-and-tests.html
+    @ParameterizedTest
+    @CsvSource({    "0,1,1",
+                    "1,2,3",
+                    "-2,2,0",
+                    "0,0,0",
+                    "-1,-2,-3"
+    })
+    void add_devrait_calculer_la_sum_de_diff_additions(int opG, int opD, int resultat_attendu) {
+
+        // WHEN
+        int resultat_class = calc.add(opG, opD);
 
         // THEN
         assertThat(resultat_class).isNotNull()
